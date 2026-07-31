4ed5155234c08ba6f473668214c3621dd8cade62
Uttam Gawas <uttam@whizti.com>
2026-07-31 19:09:49 +0530

    dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Login from './components/Auth/Login'
-import { EmployeeDashboard } from './components/Dashboard/EmployeeDashboard'
+import AdminDashBoard from './components/Dashboard/AdminDashBoard'
+import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 
 const App = () => {
   return (
   <>
       {/* <Login /> */}
-      <EmployeeDashboard />
+      {/* <EmployeeDashboard /> */}
+      {/* <AdminDashBoard /> */}
   </>
   )
 }
--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -3,12 +3,15 @@ import  Header  from '../other/Header'
 import  TaskListNumbers  from '../other/TaskListNumbers'
 import  TaskList  from '../TaskList/TaskList'
 
-export const EmployeeDashboard = () => {
+const EmployeeDashboard = () => {
   return (
     <div className='p-10 bg-[#1C1C1C] h-screen'>
         <Header />
         <TaskListNumbers />
-        <TaskList />
+        <TaskList /> 
     </div>
   )
 }
+
+
+export default EmployeeDashboard
