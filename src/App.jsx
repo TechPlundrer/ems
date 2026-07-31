8a9b4843b3d1eab0a0dfc4c7fbfc316553c96bb8
Uttam Gawas <uttam@whizti.com>
2026-07-31 22:51:36 +0530

    data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Login from './components/Auth/Login'
 import AdminDashBoard from './components/Dashboard/AdminDashBoard'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
+import { setLocalStorage } from './utils/localStorage'
+
 
 const App = () => {
+
+  useEffect (() => {
+    setLocalStorage ()
+  },)
+  
   return (
   <>
-      {/* <Login /> */}
+      <Login />
       {/* <EmployeeDashboard /> */}
       {/* <AdminDashBoard /> */}
   </>
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,1 +1,9 @@
+ import React from 'react'
  
+ const AuthContext = ({children}) => {
+   return (
+     <div>{children}</div>
+   )
+ }
+ 
+ export default AuthContext
