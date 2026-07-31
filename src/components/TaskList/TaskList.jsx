8151226d859585986187c8939d5b4af7193e0090
Uttam Gawas <uttam@whizti.com>
2026-07-31 16:58:41 +0530

    tasklist

---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,91 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div id='tasklist' className='h-[55%] overflow-x-auto flex items-center justify-start gap-5 fles-nowrap w-full py-5 mt-10'>
+        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-orange-400 rounded-xl'>
+               <div className='flex justify-between item-center'>
+                    <h3 className='bg-orange-600 text-sm px-3 py-1 rounded'>High</h3>
+                    <h4 className='text-sm'>21 may 2026</h4>
+               </div>
+               <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+               <p className='text-sm mt-2'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem vel maxime eius ratione deserunt eum velit blanditiis sapiente consectetur esse aperiam reiciendis rem corporis ex, omnis consequatur animi delectus dignissimos.
+               </p>
+        </div>
+        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-green-400 rounded-xl'>
+               <div className='flex justify-between item-center'>
+                    <h3 className='bg-orange-600 text-sm px-3 py-1 rounded'>High</h3>
+                    <h4 className='text-sm'>21 may 2026</h4>
+               </div>
+               <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+               <p className='text-sm mt-2'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem vel maxime eius ratione deserunt eum velit blanditiis sapiente consectetur esse aperiam reiciendis rem corporis ex, omnis consequatur animi delectus dignissimos.
+               </p>
+        </div>
+        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-purple-400 rounded-xl'>
+               <div className='flex justify-between item-center'>
+                    <h3 className='bg-orange-600 text-sm px-3 py-1 rounded'>High</h3>
+                    <h4 className='text-sm'>21 may 2026</h4>
+               </div>
+               <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+               <p className='text-sm mt-2'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem vel maxime eius ratione deserunt eum velit blanditiis sapiente consectetur esse aperiam reiciendis rem corporis ex, omnis consequatur animi delectus dignissimos.
+               </p>
+        </div>
+        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl'>
+               <div className='flex justify-between item-center'>
+                    <h3 className='bg-orange-600 text-sm px-3 py-1 rounded'>High</h3>
+                    <h4 className='text-sm'>21 may 2026</h4>
+               </div>
+               <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+               <p className='text-sm mt-2'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem vel maxime eius ratione deserunt eum velit blanditiis sapiente consectetur esse aperiam reiciendis rem corporis ex, omnis consequatur animi delectus dignissimos.
+               </p>
+        </div>
+        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-orange-400 rounded-xl'>
+               <div className='flex justify-between item-center'>
+                    <h3 className='bg-orange-600 text-sm px-3 py-1 rounded'>High</h3>
+                    <h4 className='text-sm'>21 may 2026</h4>
+               </div>
+               <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+               <p className='text-sm mt-2'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem vel maxime eius ratione deserunt eum velit blanditiis sapiente consectetur esse aperiam reiciendis rem corporis ex, omnis consequatur animi delectus dignissimos.
+               </p>
+        </div>
+        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-green-400 rounded-xl'>
+               <div className='flex justify-between item-center'>
+                    <h3 className='bg-orange-600 text-sm px-3 py-1 rounded'>High</h3>
+                    <h4 className='text-sm'>21 may 2026</h4>
+               </div>
+               <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+               <p className='text-sm mt-2'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem vel maxime eius ratione deserunt eum velit blanditiis sapiente consectetur esse aperiam reiciendis rem corporis ex, omnis consequatur animi delectus dignissimos.
+               </p>
+        </div>
+        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-purple-400 rounded-xl'>
+               <div className='flex justify-between item-center'>
+                    <h3 className='bg-orange-600 text-sm px-3 py-1 rounded'>High</h3>
+                    <h4 className='text-sm'>21 may 2026</h4>
+               </div>
+               <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+               <p className='text-sm mt-2'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem vel maxime eius ratione deserunt eum velit blanditiis sapiente consectetur esse aperiam reiciendis rem corporis ex, omnis consequatur animi delectus dignissimos.
+               </p>
+        </div>
+        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl'>
+               <div className='flex justify-between item-center'>
+                    <h3 className='bg-orange-600 text-sm px-3 py-1 rounded'>High</h3>
+                    <h4 className='text-sm'>21 may 2026</h4>
+               </div>
+               <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+               <p className='text-sm mt-2'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem vel maxime eius ratione deserunt eum velit blanditiis sapiente consectetur esse aperiam reiciendis rem corporis ex, omnis consequatur animi delectus dignissimos.
+               </p>
+        </div>
+       
+    </div>
+  )
+}
+
+export default TaskList
